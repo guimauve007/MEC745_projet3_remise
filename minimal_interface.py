fd2a591010cb71b3e8b470f7030b6aa4006e0fcd
geremy <geremysauvageau@gmail.com>
2025-03-19 12:37:02 -0400

    clean up code

--- minimal_interface.py
+++ minimal_interface.py
@@ -34,24 +34,30 @@ rate = rospy.Rate(50)
 
 # *************************************************************************************************
 
-# Global variables
-# Definir les variables globales ici
-# Control gains
-Kp_angular = 1  # Proportional gain for angular velocity
-Kp_linear = 0.04  # Proportional gain for linear velocity
-min_linear_speed = 0.75 # Minimum speed for slow approach
-MAX_LINEAR_SPEED = 0.5  # Maximum forward speed
-MAX_ANGULAR_SPEED = 0.5
-angular_speed_limit = 1  # Limit for angular velocity
-linear_velocity = 0
-angular_speed = 0
+# paramètres du suivi de trajectoire
+KP_ANGULAR = 1  # Gain proportionnel vitesse angulaire
+KP_LINEAR = 0.04  # Gain proportionnel vitesse linéaire
+MIN_LINEAR_SPEED = 0.15 # m/s
+MAX_LINEAR_SPEED = 0.5 # m/s
+MIN_ANGULAR_SPEED = 0.05 # rad/s
+MAX_ANGULAR_SPEED = 0.5 # rad/s
+TOLERANCE_ANGLE = 0.05 # rad
+TOLERANCE_DISTANCE = 0.05 # m
 
-scaling = 1/8
-origin_x = 1444.7
-origin_y = 1052.1
+# paramètres du robot
+INTERWHEEL_DISTANCE = 0.434 # m
+LEFT_WHEEL_RADIUS = 0.049 # m
+RIGHT_WHEEL_RADIUS = 0.049 # m
 
-map_offset_x = origin_x * scaling
-map_offset_y = origin_y * scaling
+# Vitesses pour le déplacement manuel
+MANUAL_LINEAR_SPEED = 0.5 # m/s
+MANUAL_ANGULAR_SPEED = 0.5 # rad/s
+
+# Paramètres de la map
+MAP_SCALING = 1/8
+ROBOT_MAP_OFFSET_X = 1444.7 # position en X du robot dans la map d'origine (en pixels)
+ROBOT_MAP_OFFSET_Y = 1052.1 # position  en Y du robot dans la map d'origine (en pixels)
+
 
 # path planing
 image = mpimg.imread("/home/jupyter-mecbotg11/Project5/Maps/a2230_map_closed.png")
@@ -66,12 +72,14 @@ def cam_callback(msg):
     global cam_msg
     cam_msg = msg
 
+
 # Odometry subscriber callback
 odom_msg = Odometry()
 def odometry_callback(msg):
     global odom_msg
     odom_msg = msg
     # print(f"position x: {msg.pose.pose.position.x}, position y: {msg.pose.pose.position.y}")
+
 
 # ROS subscribers et publishers
 if SIM:
@@ -88,6 +96,7 @@ def updateTargetDestinationDisplay(x_clicked, y_clicked):
     x_clicked_value.set('{:2f}'.format(x_clicked))
     y_clicked_value.set('{:2f}'.format(y_clicked))
 
+
 ## CONVERSIONS (a mettre dans un autre fichier .py?)
 def convert_pixel_pos_to_distance(x_clicked, y_clicked):
     # Convertir de position de pixel a une distance en m
@@ -95,6 +104,7 @@ def convert_pixel_pos_to_distance(x_clicked, y_clicked):
     y_distance = 0.02*y_clicked
     
     return x_distance, y_distance
+
 
 def convert_pos_to_pixel_distance(x_meter, y_meter):
     # Convertir de distance en m a position de pixel
@@ -105,28 +115,19 @@ def convert_pos_to_pixel_distance(x_meter, y_meter):
     return x_pixel, y_pixel
 
 
-
 def get_robot_map_pixel_position():
-    #odometry_msg = Odometry()
-
-    # http://docs.ros.org/en/noetic/api/nav_msgs/html/msg/Odometry.html
-    # odometry_msg.header.frame_id = "odom"
-    # odometry_msg.header.stamp = rospy.Time.now()
-    # odometry_msg.child_frame_id = "base_link"
-    # odometry_msg.pose.pose = Pose(Point(*pose), Quaternion(*orientation))
-    # odometry_msg.twist.twist = Twist(Vector3(vlin, 0, 0), Vector3(0, 0, vang))
-    # print(odometry_msg.pose.pose)
+ 
     robot_x, robot_y = convert_pos_to_pixel_distance(odom_msg.pose.pose.position.x, odom_msg.pose.pose.position.y)
-
     q = odom_msg.pose.pose.orientation  # Quaternion
     theta = get_heading_from_quaternion(q)
-    # print(f"robot position:{robot_x}, {robot_y}, {theta}")
+
     return robot_x, robot_y, theta
+
 
 def create_path(x, y):
 
     start_x, start_y, theta = get_robot_map_pixel_position()
-    start = Point(start_x+origin_x, start_y+origin_y)
+    start = Point(start_x+ROBOT_MAP_OFFSET_X, start_y+ROBOT_MAP_OFFSET_Y)
     end = Point(x, y)
     print(f"start x: {start.x}, end x: {end.x}")
     print(f"start y: {start.y}, end y: {end.y}")
@@ -135,95 +136,71 @@ def create_path(x, y):
 
     for i in range(len(astarPlanner.finalPath)-1):
         pt = astarPlanner.finalPath[i].tuple()
-        print(pt)
+        print(pt) # Imprime la liste de positions du trajet
 
 
-def compute_heading_error(current_x, current_y, current_theta, target_x, target_y):
-    """ Compute the error in heading (angular error) towards the target. """
-    desired_theta = atan2(target_y - current_y, target_x - current_x)
-    heading_error = desired_theta - current_theta
+def get_heading_error(current_x, current_y, current_theta, target_x, target_y):
+    
+    target_theta = atan2(target_y - current_y, target_x - current_x)
+    heading_error = target_theta - current_theta
     
     return heading_error
 
-def compute_distance(x1, y1, x2, y2):
-    """ Compute Euclidean distance between two points. """
+
+def get_waypoint_distance(x1, y1, x2, y2):
+    
     return sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
 
 # Fonction pour le calcul de l'orientation à partir d'un quaternion
 # Entrée : Quaternion [x, y ,z ,w]
 # Sortie : Angle de lacet (yaw) en radians
 def get_heading_from_quaternion(q):
+    
     r = R.from_quat([q.x, q.y, q.z, q.w])
     angles = r.as_euler('xyz', degrees=False)
     return angles[2]
 
 
 def follow_path(path):
-    #Paramètres
 
-    tolerance_angle = 0.05
+    # position actuelle du robot
+    waypoint_index = 0
+    while waypoint_index != len(path) :
 
-    linear_min = 0.15
+        waypoint_x=path[waypoint_index].x - ROBOT_MAP_OFFSET_X
+        waypoint_y=-(path[waypoint_index].y - ROBOT_MAP_OFFSET_Y)
 
-    #print(liste_traj2)
-    #position reel du robot 
-    i = 0
-    while i != len(path) :
-
-        pos_x=path[i].x - origin_x
-        pos_y=-(path[i].y - origin_y)
-
-        #position reel du robot 
+        # position actuelle du robot 
         robot_x, robot_y, robot_cap = get_robot_map_pixel_position()
 
-        #print(cap_du_robot)
+        waypoint_distance = get_waypoint_distance(robot_x, robot_y, waypoint_x, waypoint_y)
+        waypoint_angle = get_heading_error(robot_x, robot_y, robot_cap, waypoint_x, waypoint_y)
 
-        deplacement_x= pos_x-robot_x
-        deplacement_y= pos_y-robot_y
-
-        if np.sqrt(deplacement_x**2+deplacement_y**2) < 0.05 /0.02:
-            i +=1
-            print(f"waypoint number: {i}")
+        if waypoint_distance < TOLERANCE_DISTANCE / 0.02:
+            waypoint_index +=1
+            print(f"waypoint number: {waypoint_index}")
             print(f"robot position: {robot_x}, {robot_y}")
             continue
 
-        linear = np.sqrt(deplacement_x**2+deplacement_y**2)*Kp_linear
-        if linear < linear_min:
-            linear = linear_min
+        linear = min(waypoint_distance*KP_LINEAR, MIN_LINEAR_SPEED)
             
-        angular = (np.arctan2(deplacement_y,deplacement_x) - robot_cap)*Kp_angular
-            
-        #Comparaison des angles
-        # difference_angle = robot_cap-angular
-        # angle_cible = np.arctan2(deplacement_y,deplacement_x)
-        difference_angle = np.arctan2(deplacement_y,deplacement_x) - robot_cap
+        angular = waypoint_angle*KP_ANGULAR
         
-        #print(f"Cap du robot : {robot_cap}, Angle cible : {angle_cible}, Différence d'angle : {difference_angle}")
-
         # Si l'angle est suffisamment proche de la cible, on peut se déplacer linéairement
-        if abs(difference_angle) < tolerance_angle:
+        if abs(waypoint_angle) < TOLERANCE_ANGLE:
             #print("Le robot est correctement orienté, déplacement linéaire.")
             move_robot(linear, angular)
         else:
             #print(f"Le robot ajuste son orientation. Différence d'angle : {difference_angle}")
             move_robot(0, angular)  # Correction de l'orientation
             
-        #print (linear)
         rospy.sleep(0.1)
         
 
-## DÉPLACEMENT DU ROBOT
-# mesures
-interwheel_distance = 0.434
-left_wheel_radius = 0.049
-right_wheel_radius = 0.049
-# valeurs de vitesses pour les boutons
-linear_speed = 0.5
-angular_speed = 0.5
-
-def move_robot(linear, angular):
-    vel_left  = (linear - angular * interwheel_distance / 2.0) / left_wheel_radius
-    vel_right = (linear + angular * interwheel_distance / 2.0) / right_wheel_radius
+def move_robot(linear_speed, angular_speed):
+    vel_left  = (linear_speed - angular_speed * INTERWHEEL_DISTANCE / 2.0) / LEFT_WHEEL_RADIUS
+    vel_right = (linear_speed + angular_speed * INTERWHEEL_DISTANCE / 2.0) / RIGHT_WHEEL_RADIUS
 
     # Envoi des commandes au roues par topic ROS
     cmd_drive_msg = Drive()
@@ -274,10 +251,10 @@ label.pack()
 buttons_frame = tk.Frame(root)
 buttons_frame.pack()
 
-btn_forward = tk.Button(buttons_frame, text="Forward", command=lambda: move_robot(linear_speed, 0))
-btn_left = tk.Button(buttons_frame, text="Left", command=lambda: move_robot(0, angular_speed))
-btn_backward = tk.Button(buttons_frame, text="Backward", command=lambda: move_robot(-linear_speed, 0))
-btn_right = tk.Button(buttons_frame, text="Right", command=lambda: move_robot(0, -angular_speed))
+btn_forward = tk.Button(buttons_frame, text="Forward", command=lambda: move_robot(MANUAL_LINEAR_SPEED, 0))
+btn_left = tk.Button(buttons_frame, text="Left", command=lambda: move_robot(0, MANUAL_ANGULAR_SPEED))
+btn_backward = tk.Button(buttons_frame, text="Backward", command=lambda: move_robot(-MANUAL_LINEAR_SPEED, 0))
+btn_right = tk.Button(buttons_frame, text="Right", command=lambda: move_robot(0, -MANUAL_ANGULAR_SPEED))
 btn_stop = tk.Button(buttons_frame, text="Stop", command=lambda: move_robot(0,0))
 
 btn_forward.pack()
@@ -288,7 +265,7 @@ btn_backward.pack()
 
 # Load map image
 map_image = Image.open("/home/jupyter-mecbotg11/Project5/Maps/a2230_map_closed.png")
-map_image = map_image.resize((int(map_image.width * scaling), int(map_image.height * scaling)))  # Resize the image
+map_image = map_image.resize((int(map_image.width * MAP_SCALING), int(map_image.height * MAP_SCALING)))  # Resize the image
 map_photo = ImageTk.PhotoImage(map_image)
 
 # Canvas for map
@@ -298,8 +275,8 @@ def on_map_click(event):
     updateTargetDestinationDisplay(x_clicked, y_clicked)
     #move_robot(distance_to_linear_angular(convert_pixel_pos_to_distance()))  
     #move_robot(0.5, 0) # Example movement command
-    x_click_scaled = x_clicked/scaling
-    y_click_scaled = y_clicked/scaling
+    x_click_scaled = x_clicked/MAP_SCALING
+    y_click_scaled = y_clicked/MAP_SCALING
     create_path(x_click_scaled, y_click_scaled)
     follow_path(astarPlanner.finalPath)
     
